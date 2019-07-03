f852910fb7c506ef903765f312b03f73641d234d
qiujia <ooiqiujia@hotmail.com>
2019-07-03 23:55:50 +0800

    update

--- app/src/main/java/com/example/stockup/BuyerShoppingListActivity.java
+++ app/src/main/java/com/example/stockup/BuyerShoppingListActivity.java
@@ -1,9 +1,13 @@
 package com.example.stockup;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,6 +28,7 @@ public class BuyerShoppingListActivity extends AppCompatActivity {
 
     List<Groceries> list;
     ListView myListView;
+    Button myButtonConfirmOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,16 @@ public class BuyerShoppingListActivity extends AppCompatActivity {
         user = firebaseAuth.getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference("users");
         myListView = (ListView) findViewById(R.id.buyer_shopping_list);
+        myButtonConfirmOrder = (Button) findViewById(R.id.buyer_shopping_list_order);
+
+        myButtonConfirmOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BuyerShoppingListActivity.this, "Order successfully sent", Toast.LENGTH_SHORT).show();
+                Intent goToOrders = new Intent(BuyerShoppingListActivity.this, BuyerOrderActivity.class);
+                startActivity(goToOrders);
+            }
+        });
 
         list = new ArrayList<>();
 
