0aea565ed2c4acdfbcbdf79ff072fccfc7990d02
qiujia <ooiqiujia@hotmail.com>
2019-07-04 00:29:17 +0800

    update

--- app/src/main/java/com/example/stockup/BuyerOrderConfirmationActivity.java
+++ app/src/main/java/com/example/stockup/BuyerOrderConfirmationActivity.java
@@ -1,17 +1,24 @@
 package com.example.stockup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class BuyerOrderConfirmationActivity extends Activity {
+
+    Button myButtonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buyer_order_confirmation);
+
+        myButtonSend = (Button) findViewById(R.id.buyer_order_confirmation);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -19,9 +26,14 @@ public class BuyerOrderConfirmationActivity extends Activity {
         int height = dm.heightPixels;
         getWindow().setLayout((int) (width * 0.6), (int) (height * 0.4));
 
-
-        Toast.makeText(BuyerOrderConfirmationActivity.this, "Order successfully sent", Toast.LENGTH_SHORT).show();
-
+        myButtonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BuyerOrderConfirmationActivity.this, "Order successfully sent", Toast.LENGTH_SHORT).show();
+                Intent goToOrder = new Intent(BuyerOrderConfirmationActivity.this, BuyerOrderActivity.class);
+                startActivity(goToOrder);
+            }
+        });
     }
 
 
